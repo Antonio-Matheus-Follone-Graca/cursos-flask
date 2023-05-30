e9f02f0e382d489cddb8e65245bb848ddf04c0ae
Antônio Matheus Follone Graça <98711863+Antonio-Matheus-Follone-Graca@users.noreply.github.com>
2023-05-30 10:59:32 -0300

    Formulário

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,17 +1,54 @@
 # importando flask
-from flask import Flask, render_template 
+from flask import Flask, render_template, request, redirect
+
  
+ # criando classe
+class Jogo:
+    def __init__(self,nome,categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
+#
+jogo1= Jogo('Tetris','Puzzle','Atati')
+jogo2= Jogo('God of war','Hack n slash','Ps2')
+jogo3= Jogo('Mortal Kombat','Luta','Ps2')
+lista = [jogo1,jogo2,jogo3]
 
 app = Flask(__name__)
 
 # criando uma rota 
 # ao  acessar /inicio na url a função retorna o seguinte código
-@app.route('/inicio')
+# rota index
+@app.route('/')
 # funcao para a rota
-def ola():
+def index():
     # renderizando página html da pasta template com o render_template
-    lista = ['Tetris', 'Skyrim', 'Crash']
     return render_template('lista.html', titulo= 'Jogos',  jogos=lista) # passando variável para o lista.html
 
-# rodando aplicação
-app.run()
+# rota para página de  cadastro
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo = 'Novo jogo')
+
+
+
+# rota para cadastro
+@app.route('/criar', methods=['POST',])
+def criar():
+    
+    # request pega as informações de um formulário 
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    # passando para objeto
+    jogo= Jogo(nome,categoria,console)
+    # passando para a lista de array
+    lista.append(jogo)
+    # redirecionando para a rota index
+    return redirect('/')
+    
+    
+# rodando aplicação e ativando debug para a apliacação ficar se atualizando toda hora    
+app.run(debug=True)
