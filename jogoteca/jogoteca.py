721ceac4abe2fdc98cfba5409b363b1cab4b60d1
Antônio Matheus Follone Graça <98711863+Antonio-Matheus-Follone-Graca@users.noreply.github.com>
2023-05-29 23:44:46 -0300

    Criando primeira página html no projeto

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,14 +1,16 @@
 # importando flask
-from flask import Flask
+from flask import Flask, render_template 
+ 
 
 app = Flask(__name__)
 
 # criando uma rota 
-
+# ao  acessar /inicio na url a função retorna o seguinte código
 @app.route('/inicio')
 # funcao para a rota
 def ola():
-    return '<h1> Olá mundo </h1>'
+    # renderizando página html da pasta template com o render_template
+    return render_template('lista.html', titulo= 'Jogos') # passando variável para o lista.html
 
 # rodando aplicação
 app.run()
